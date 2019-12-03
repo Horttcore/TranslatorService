4914e07281b85776959e3bba2c7e8a2d6a127e03
Ralf Hortt <me@horttcore.de>
2019-12-03 15:59:30 +0100

    Remove test file
    Signed-off-by: Ralf Hortt <me@horttcore.de>

--- index.php
+++
@@ -1,7 +0,0 @@
-<?php
-use RalfHortt\TranslatorService\Translator;
-
-include('vendor/ralfhortt/service-contracts/src/ServiceContract.php');
-include('src/Translator.php');
-
-$translator = (new Translator('asdads', 'blablubb'))->register();
